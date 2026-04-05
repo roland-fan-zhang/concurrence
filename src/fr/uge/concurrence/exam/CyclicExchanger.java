f66652c0c54e446769ff05fe939745e54969081f
Roland <roland.zhang.pro@gmail.com>
2026-04-05 17:48:24 +0200

    cyclic exchanger

---
+++ src/fr/uge/concurrence/exam/CyclicExchanger.java
@@ -0,0 +1,52 @@
+package fr.uge.concurrence.exam;
+
+public class CyclicExchanger<T> {
+  private final Object lock = new Object();
+
+  private final T[] values;
+  private final int nbParticipants;
+  private int counter;
+  private boolean ready;
+
+  @SuppressWarnings("unchecked")
+  public CyclicExchanger(int nbParticipants){
+    this.nbParticipants = nbParticipants;
+    this.values = (T[]) new Object[nbParticipants];
+  }
+
+  public T exchange(T value) throws InterruptedException {
+    synchronized (lock){
+      if (ready) {
+        throw new IllegalStateException();
+      }
+      var index = counter;
+      values[counter] = value;
+      counter++;
+      if(counter == nbParticipants){
+        ready = true;
+        lock.notifyAll();
+      }
+      while(!ready){
+        lock.wait();
+      }
+      return values[(index + 1) % nbParticipants];
+    }
+  }
+
+  static void main() {
+    var exchanger = new CyclicExchanger<Integer>(5);
+
+    for (int i = 0; i < 5; i++) {
+      final int threadIndex = i;
+      Thread.ofPlatform().start(() -> {
+        try {
+          Thread.sleep(threadIndex * 1_000L);
+          var result = exchanger.exchange(threadIndex);
+          System.out.println("Thread " + threadIndex + " a reçu : " + result);
+        } catch (InterruptedException e) {
+          throw new AssertionError(e);
+        }
+      });
+    }
+  }
+}
